a6d40c00fe7b9eccc2813760f0933937d1aaff2f
unknown <anshulkashyap118@gmail.com>
2026-02-04 16:43:06 +0530

    function passing onclick

--- React/Props/src/props/props.jsx
+++ React/Props/src/props/props.jsx
@@ -4,7 +4,9 @@ const Props = (props) => {
 
   return (
     <div>
+
       <h2>{props.name}</h2>
+
       <h2>Technologies List</h2>
 
       <ul>
@@ -12,14 +14,32 @@ const Props = (props) => {
           <li key={i}>{item}</li>
         ))}
       </ul>
+
       <h2>User Details</h2>
-      <p>Name:{props.obj.name}</p>
-      <p>Age:{props.obj.age}</p>
+
+      <p>Name: {props.obj.name}</p>
+      <p>Age: {props.obj.age}</p>
+
       <ul>
-        {props.obj.arr1.map((item,i)=>(
+        {props.obj.arr1.map((item, i) => (
           <li key={i}>{item}</li>
         ))}
       </ul>
+
+      <h2>Students List (Array of Objects)</h2>
+
+      <ul>
+        {props.students.map((stu) => (
+          <li key={stu.id}>
+            {stu.name} - {stu.marks}
+          </li>
+        ))}
+      </ul>
+
+     <button onClick={() => props.sum(3,4)}>
+
+        Show Sum
+      </button>
 
     </div>
   )
