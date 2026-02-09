7c62b97bf79cfe77da608cb2d6ae38e66dce1518
unknown <anshulkashyap118@gmail.com>
2026-02-10 00:18:44 +0530

    forms with cards

--- JS Practice/DOM/Dom1/script.js
+++ JS Practice/DOM/Dom1/script.js
@@ -12,11 +12,28 @@
 
 
 //Change event 
-let sel = document.querySelector("select")
-let head = document.querySelector("#s")
+// let sel = document.querySelector("select")
+// let head = document.querySelector("#s")
 
-sel.addEventListener("change",function(dets)
-{
-head.textContent = `${dets.target.value} Device  is selected`
-})
-console.log
+// sel.addEventListener("change",function(dets)
+// {
+// head.textContent = `${dets.target.value} Device  is selected`
+// })
+// console.log
+
+
+// Keydown
+// let h1 = document.querySelector("h1")
+// window.addEventListener("keydown",function(e){
+//   if(e.key===" "){
+//     h1.textContent = "SPC"
+//   }
+//   else
+//   {
+//   h1.textContent = e.key;
+//   }
+
+// })
+
+
+
