36984e7a5458facd1ffe09863865815827aa440f
AnshulKumar21 <anshulkashyap118@gmail.com>
2026-04-29 02:36:43 +0530

    useeffect and live api

--- React/Namste React/FoodOrderingApp/src/components/Body.js
+++ React/Namste React/FoodOrderingApp/src/components/Body.js
@@ -1,16 +1,36 @@
 import RestaurantCard from "./Resaurantcard";
 import resList from "../utils/mockdata";
-import { useState } from "react";
+import { use, useState } from "react";
+import { useEffect } from "react";
 const Body = () => {
 //State variable
-const arr = useState(resList)
+const arr = useState([])
 // const [ListOfRes,setListOfRes] = useState(resList);
 //destructuring
 // const [ListOfRes,setListOfRes] = arr;
 const ListOfRes = arr[0];
 const setListOfRes = arr[1];
 //Normal var;
-  
+useEffect(() => {
+  console.log("called");
+  fetchData();
+}, []);
+  const fetchData =async ()=>{
+    console.log("fetching")
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json)
+   setListOfRes(
+  json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+);
+ };
+ if(ListOfRes.length===0)
+ {
+  return<h1>Loading....</h1>
+
+ }
   return (
     <div className="body">
       <div className="filter">
